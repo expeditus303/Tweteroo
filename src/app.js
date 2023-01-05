6642e59451640c64878b9d3a48e0bafe9ab53c30
expeditus303 <ricardotonietto@gmail.com>
2023-01-05 17:37:10 -0300

    running server

--- src/app.js
+++ src/app.js
@@ -1,1 +1,11 @@
-console.log('hello, world!')
+import express from 'express'
+
+const server = express()
+
+server.listen(5000, () => {
+    console.log('server is running')
+})
+
+server.get("/", (req, res) => {
+    res.send("hello world")
+})
