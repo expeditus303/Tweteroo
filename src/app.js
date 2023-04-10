f14544dc4e394886e803ab9252a661142e96196b
expeditus303 <ricardotonietto@gmail.com>
2023-04-10 13:20:25 -0300

    feat/pagination

--- src/app.js
+++ src/app.js
@@ -3,7 +3,18 @@ import cors from "cors";
 
 const users = [];
 
-const tweets = [];
+const tweets = []
+
+for (let i = 0; i < 100; i++) {
+  const tweet =   {
+    username: "ana",
+    avatar: "https://img.elo7.com.br/product/zoom/22B5B61/adesivo-redondo-sandy-bob-esponja-sandy-do-bobo-esponja.jpg",
+    tweet: `eba o ric vai!!!! ${i}`
+  }
+  tweets.push(tweet)
+}
+
+tweets.reverse()
 
 const app = express();
 app.use(cors());
@@ -16,45 +27,50 @@ app.listen(PORT, () => {
 });
 
 app.post("/sign-up", (req, res) => {
+  const { username, avatar } = req.body;
+
   if (
-    req.body.username &&
-    req.body.avatar &&
-    typeof req.body.username == "string" &&
-    typeof req.body.avatar == "string" &&
+    username &&
+    avatar &&
+    typeof username == "string" &&
+    typeof avatar == "string" &&
     Object.keys(req.body).length == 2
   ) {
     users.push(req.body);
-    res.status(201).send('OK')
+    res.status(201).send("OK");
   } else {
     res.status(400).send("Todos os campos são obrigatórios!");
   }
 });
 
 app.get("/tweets", (req, res) => {
-  res.send(tweets);
+
+  const page = req.query.page
+
+  if (page && page < 1) return res.status(400).send("Informe uma página válida")
+
+  const limit = 10
+  const start = (page - 1) * limit
+  const end = page * limit
+  
+  if (page) {
+    return res.send(tweets.slice(start, end))
+  }
+  res.send(tweets.slice(0, 10));
 });
 
 app.post("/tweets", (req, res) => {
-  console.log(req.headers)
 
- 
-  if (
-    req.headers.user &&
-    req.body.tweet &&
-    typeof req.body.tweet == "string"
-  ) {
+  if (req.headers.user && req.body.tweet && typeof req.body.tweet == "string") {
     const user = users.find((user) => user.username == req.headers.user);
 
     if (user) {
-      tweets.push({
+      tweets.unshift({
         username: user.username,
         avatar: user.avatar,
         tweet: req.body.tweet,
       });
-      if (tweets.length > 10) {
-        tweets.shift();
-      }
-      res.status(201).send('OK')
+      res.status(201).send("OK");
     } else {
       res.sendStatus(401); // Unauthorized
     }
@@ -64,9 +80,9 @@ app.post("/tweets", (req, res) => {
 });
 
 app.get("/tweets/:username", (req, res) => {
-  const user = req.params.username
+  const user = req.params.username;
 
-  const userTweets = tweets.filter((t) => t.username == user)
-  
-  res.send(userTweets)
-})
+  const userTweets = tweets.filter((t) => t.username.toLowerCase == user.toLowerCase);
+
+  res.send(userTweets);
+});
